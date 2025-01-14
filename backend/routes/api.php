c019a815c4fe4a53556bdd5dc05b8a7e47ba49c1
Reyes Kevin <kevin.galaxy04@gmail.com>
2025-01-14 11:22:18 -0300

    feat(backend): add routes for controllers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\StationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'posts' => PostController::class,
+    'reservations' => ReservationController::class,
+    'drivers' => DriverController::class,
+    'cars' => CarController::class,
+    'reviews' => ReviewController::class,
+    'stations' => StationController::class
+]);
