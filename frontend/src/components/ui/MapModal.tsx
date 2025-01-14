6441fe6ec8c13002ab5f4e9b79919a61e4828b96
Mateo Dans <mateodand@gmail.com>
2025-01-14 16:00:52 -0300

    fix(MapModal): update icon URL imports to use dynamic import syntax for Leaflet icons

--- frontend/src/components/ui/MapModal.tsx
+++ frontend/src/components/ui/MapModal.tsx
@@ -6,9 +6,9 @@ import 'leaflet/dist/leaflet.css';
 // Asegúrate de que los iconos de Leaflet se cargan correctamente
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: import('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: import('leaflet/dist/images/marker-shadow.png'),
 });
 
 interface MapModalProps {
